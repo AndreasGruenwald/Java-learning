173082e1b53466eaae6b5dfdbac2b905cf57f9bb
AndreasGruenwald <andrewgrinwald@gmail.com>
2023-07-15 23:10:30 +0200

    polymorphism

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -2,14 +2,14 @@ package src.lesson3;
 
 public class Main {
     public static void main(String[] args) {
-        Main main = new Main();
-        main.useCar(new Car()); //Car car = new Car();
-        main.useCar(new Toyota());
-        main.useCar(new Bmw());
-        main.useCar(new Lamborghini());
+        //Main main = new Main();
+        useCar(new Car()); //Car car = new Car();
+        useCar(new Toyota());
+        useCar(new Bmw());
+        useCar(new Lamborghini());
     }
 
-    void useCar(Car car) {
+    static void useCar(Car car) {
         System.out.println(car.getNameOfCar());
         car.driving();
         System.out.println("count of seats: " + car.getNumOfSeats());
