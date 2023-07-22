71ec116e5bd55af30f4a012a75a4fcc876e84eb0
AndreasGruenwald <andrewgrinwald@gmail.com>
2023-07-22 02:13:06 +0200

    Lotto

--- src/Lotto/Card.java
+++ src/Lotto/Card.java
@@ -9,7 +9,7 @@ public class Card {
     private int countOfBarrelsInCard = 27;
     private int countOfBarrelsInGame = 90;
     private int[][] card = new int[LINES][ROWS];
-
+    
     private void createCard() {
         cardSet();
         addZeroes();
@@ -24,13 +24,13 @@ public class Card {
     }  //for createCard();
     
     private void addZeroes() {
-        /*for (int i = 0; i < LINES; i++) {
-            var arrayOfRandomCounters = arrayOfRandomCountersSet();
+        for (int i = 0; i < LINES; i++) {
+            var arrayOfRandomCounts = arrayOfRandomCountsSet();
             for (int j = 0; j < 4; j++) {
-                card[i][arrayOfRandomCounters[j]] = 99;
+                card[i][arrayOfRandomCounts[j]] = 99;
             }
-        }*/
-    }  //for createCard(); not ready
+        }
+    }  //for createCard();
     
     private int createRandomBarrel(int i_Iteration, int j_Iteration) {
         Random random = new Random();
@@ -43,7 +43,7 @@ public class Card {
             randomCount = random.nextInt(10);
         }
         
-        if (Check(i_Iteration, j_Iteration, randomCount)) {
+        if (diversityCheckForCardSet(i_Iteration, j_Iteration, randomCount)) {
             passedCount = randomCount;
         }
         else {
@@ -52,23 +52,45 @@ public class Card {
         return j_Iteration * 10 + passedCount;
     }  //for cardSet();
     
-    private boolean Check(int i_Iteration, int j_Iteration, int randomCount) {
+    private boolean diversityCheckForCardSet(int i_Iteration, int j_Iteration, int randomCount) {
         for (int i = 0; i < i_Iteration; i++) {
             if (j_Iteration * 10 + randomCount == card[i][j_Iteration]) {
                 return false;
             }
         }
         return true;
-    }  // for cardSet();
+    }  // for cardSet().createRandomBarrel()
     
-    private int[] arrayOfRandomCountersSet() {
-        /*var arrayOfRandomCounters = new int[4];
+    private int[] arrayOfRandomCountsSet() {
+        var arrayOfRandomCounts = new int[4];
+
+        for (int i = 0; i < arrayOfRandomCounts.length; i++) {
+            arrayOfRandomCounts[i] = randomCountSet(arrayOfRandomCounts);
+        }
+        return arrayOfRandomCounts;
+    }  //for addZeroes();
+    
+    private boolean diversityCheckForAddZeroes(int[] array, int random) {
+        for (int i = 0; i < array.length; i++) {
+            if (random == array[i]) {
+                return false;
+            }
+        }
+        return true;
+    }  //for addZeroes().arrayOfRandomCountsSet().randomCountSet()
+    
+    private int randomCountSet(int[] arrayOfRandomCounts) {
         Random random = new Random();
-        for (int i = 0; i < arrayOfRandomCounters.length; i++) {
-            arrayOfRandomCounters[i] = random.nextInt(ROWS);
-        }*/
-        return new int[4];//arrayOfRandomCounters;
-    }  //for addZeroes(); not ready
+        int trueCount = 0;
+        var randomCount = random.nextInt(ROWS);
+        if (diversityCheckForAddZeroes(arrayOfRandomCounts, randomCount)) {
+            trueCount = randomCount;
+        }
+        else {
+            randomCountSet(arrayOfRandomCounts);
+        }
+        return trueCount;
+    }  //for addZeroes().arrayOfRandomCountsSet()
     
     public int[][] getCard() {
         createCard();
@@ -90,7 +112,7 @@ public class Card {
     public int getLINES() {
         return LINES;
     }
-
+    
     private String helloWorld(){
         return "Hello World";
     }
--- src/Lotto/Program.java
+++ src/Lotto/Program.java
@@ -9,7 +9,15 @@ public class Program {
     private static void showCard(int[][] card, Card cardObject) {
         for (int i = 0; i < cardObject.getLINES(); i++) {
             for (int j = 0; j < cardObject.getROWS(); j++) {
-                System.out.print(card[i][j] + " ");
+                if (j == 0 && card[i][j] == 99) {
+                    System.out.print("_ ");
+                }
+                else if (card[i][j] == 99) {
+                    System.out.print("__ ");
+                }
+                else {
+                    System.out.print(card[i][j] + " ");
+                }
             }
             System.out.print("\n");
         }
